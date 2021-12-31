898598ef6081bd628e1cf6858224905370b077c2
nguyeduyphuonghh@gmail.com <Mjnhbg123>
2021-12-31 14:48:08 +0700

    OpenCV SDK 3.4.10

--- app/src/main/java/com/farmboy/opencvandroid/MainActivity.kt
+++ app/src/main/java/com/farmboy/opencvandroid/MainActivity.kt
@@ -4,25 +4,31 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.hardware.camera2.CameraCharacteristics
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.OrientationEventListener
 import android.view.SurfaceView
 import android.view.WindowManager
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import org.opencv.android.BaseLoaderCallback
 import org.opencv.android.CameraBridgeViewBase
 import org.opencv.android.JavaCameraView
 import org.opencv.android.OpenCVLoader
-import org.opencv.core.CvType
-import org.opencv.core.Mat
+import org.opencv.core.*
+import org.opencv.imgproc.Imgproc
+import org.opencv.objdetect.CascadeClassifier
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
 
     // view
     private val viewFinder by lazy { findViewById<JavaCameraView>(R.id.cameraView) }
+    private val orientationText by lazy { findViewById<TextView>(R.id.rotation_tv) }
 
     companion object {
 
@@ -46,6 +52,11 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
             Manifest.permission.RECORD_AUDIO,
             Manifest.permission.ACCESS_FINE_LOCATION
         )
+
+        // Face model
+        private const val FACE_DIR = "facelib"
+        private const val FACE_MODEL = "haarcascade_frontalface_alt2.xml"
+        private const val byteSize = 4096 // buffer size
     }
 
     private fun checkOpenCV(context: Context) =
@@ -53,8 +64,7 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
             loadEvents()
             shortMsg(context, OPENCV_SUCCESSFUL)
             Log.d(TAG, OPENCV_SUCCESSFUL)
-        }
-        else {
+        } else {
             shortMsg(context, OPENCV_FAIL)
             Log.d(TAG, OPENCV_FAIL)
         }
@@ -63,7 +73,10 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
         super.onCreate(savedInstanceState)
 
         window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
         setContentView(R.layout.activity_main)
@@ -75,7 +88,8 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
             ActivityCompat.requestPermissions(
                 this,
                 REQUIRED_PERMISSIONS,
-                REQUEST_CODE_PERMISSIONS )
+                REQUEST_CODE_PERMISSIONS
+            )
         }
     }
 
@@ -86,6 +100,7 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<String>, grantResults: IntArray
     ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
                 checkOpenCV(this)
@@ -97,37 +112,94 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
     }
 
     private fun loadEvents() {
+
+        val mOrientationEventListener = object : OrientationEventListener(this) {
+            override fun onOrientationChanged(orientation: Int) {
+                // Monitors orientation values to determine the target rotation value
+                when (orientation) {
+                    in 45..134 -> {
+                        orientationText.text = getString(R.string.n_270_degree)
+                    }
+                    in 135..224 -> {
+                        orientationText.text = getString(R.string.n_180_degree)
+                    }
+                    in 225..314 -> {
+                        orientationText.text = getString(R.string.n_90_degree)
+                    }
+                    else -> {
+                        orientationText.text = getString(R.string.n_0_degree)
+                    }
+                }
+
+            }
+        }
+        if (mOrientationEventListener.canDetectOrientation()) {
+            mOrientationEventListener.enable()
+        } else {
+            mOrientationEventListener.disable()
+        }
+
         viewFinder.visibility = SurfaceView.VISIBLE
         viewFinder.setCameraIndex(
-            CameraCharacteristics.LENS_FACING_FRONT)
+            CameraCharacteristics.LENS_FACING_FRONT
+        )
         viewFinder.setCvCameraViewListener(this)
 
         checkOpenCVManager()
     }
 
-    // openCV callback
-    lateinit var cvBaseLoaderCallback: BaseLoaderCallback
-
     private fun checkOpenCVManager() {
-        cvBaseLoaderCallback = object : BaseLoaderCallback(this) {
-            override fun onManagerConnected(status: Int) {
+        handleFaceDetect()
+        if (faceDetector!!.empty()) {
+            faceDetector = null
+        } else {
+            faceDir.delete()
+        }
+        viewFinder.enableView()
+    }
 
-                when (status) {
-                    SUCCESS -> {
-                        Log.d(TAG, OPENCV_SUCCESSFUL)
-                        shortMsg(this@MainActivity, OPENCV_SUCCESSFUL)
-                        viewFinder.enableView()
-                    }
+    // face library
+    var faceDetector: CascadeClassifier? = null
+    lateinit var faceDir: File
+    var imageRatio = 0.0 // scale down ratio
 
-                    else -> super.onManagerConnected(status)
+    private fun handleFaceDetect() {
+        try {
+            val modelInputStream =
+                resources.openRawResource(
+                    R.raw.haarcascade_frontalface_alt2
+                )
+
+            // create a temp directory
+            faceDir = getDir(FACE_DIR, Context.MODE_PRIVATE)
+
+            // create a model file
+            val faceModel = File(faceDir, FACE_MODEL)
+
+            if (!faceModel.exists()) { // copy model
+                // copy model to new face library
+                val modelOutputStream = FileOutputStream(faceModel)
+
+                val buffer = ByteArray(byteSize)
+                var byteRead = modelInputStream.read(buffer)
+                while (byteRead != -1) {
+                    modelOutputStream.write(buffer, 0, byteRead)
+                    byteRead = modelInputStream.read(buffer)
                 }
+
+                modelInputStream.close()
+                modelOutputStream.close()
             }
+
+            faceDetector = CascadeClassifier(faceModel.absolutePath)
+        } catch (e: IOException) {
+            Log.e(TAG, "Error loading cascade face model...$e")
         }
     }
 
     override fun onResume() {
         super.onResume()
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, cvBaseLoaderCallback)
+//        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, cvBaseLoaderCallback)
     }
 
     /**
@@ -139,6 +211,7 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
 
     // image storage
     lateinit var imageMat: Mat
+    lateinit var grayMat: Mat
 
     override fun onCameraViewStarted(width: Int, height: Int) {
         imageMat = Mat(width, height, CvType.CV_8UC4)
@@ -149,9 +222,79 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
     }
 
     override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame?): Mat {
-            imageMat = inputFrame!!.rgba()
-            return imageMat
+        imageMat = inputFrame!!.rgba()
+//        grayMat = inputFrame.gray()
+        grayMat = get480Image(inputFrame.gray())
+//        imageRatio = 1.0
+
+        // detect face rectangle
+        drawFaceRectangle()
+        return imageMat
     }
+
+    fun ratioTo480(src: Size): Double {
+        val w = src.width
+        val h = src.height
+        val heightMax = 480
+        var ratio: Double = 0.0
+
+        if (w > h) {
+            if (w < heightMax) return 1.0
+            ratio = heightMax / w
+        } else {
+            if (h < heightMax) return 1.0
+            ratio = heightMax / h
+        }
+
+        return ratio
+    }
+
+    fun get480Image(src: Mat): Mat {
+        val imageSize = Size(src.width().toDouble(), src.height().toDouble())
+        imageRatio = ratioTo480(imageSize)
+
+        if (imageRatio.equals(1.0)) return src
+
+        val dstSize = Size(imageSize.width * imageRatio, imageSize.height * imageRatio)
+        val dst = Mat()
+        Imgproc.resize(src, dst, dstSize)
+        return dst
+    }
+
+    private fun drawFaceRectangle() {
+        val faceRects = MatOfRect()
+        faceDetector!!.detectMultiScale(
+            grayMat,
+            faceRects
+        )
+
+        for (rect in faceRects.toArray()) {
+            var x = 0.0
+            var y = 0.0
+            var w = 0.0
+            var h = 0.0
+
+            if (imageRatio.equals(1.0)) {
+                x = rect.x.toDouble()
+                y = rect.y.toDouble()
+                w = x + rect.width
+                h = y + rect.height
+            } else {
+                x = rect.x.toDouble() / imageRatio
+                y = rect.y.toDouble() / imageRatio
+                w = x + (rect.width / imageRatio)
+                h = y + (rect.height / imageRatio)
+            }
+
+            Imgproc.rectangle(
+                imageMat,
+                Point(x, y),
+                Point(w, h),
+                Scalar(255.0, 0.0, 0.0)
+            )
+        }
+    }
+
     override fun onPause() {
         super.onPause()
         viewFinder?.let { viewFinder.disableView() }
@@ -160,5 +303,8 @@ class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListe
     override fun onDestroy() {
         super.onDestroy()
         viewFinder?.let { viewFinder.disableView() }
+        if (faceDir.exists()) faceDir.delete()
     }
+
+
 }
